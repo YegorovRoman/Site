f8754127b02019f562395b429f835a18360260ee
SitePoPrikolu <roma.yegorov.09@internet.ru>
2026-05-17 15:55:06 +0500

    update settings

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -139,5 +139,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.parent.parent / 'frontend' / 'dist' / 'assets'
+STATICFILES_DIRS = [BASE_DIR.parent.parent / 'frontend' / 'dist' / 'assets']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
